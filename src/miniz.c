a520138610fb2ee30b5a0c22d028c500beba9f4b
Franz Höpfinger <f.hoepfinger@hr-agrartechnik.de>
2025-03-15 02:42:08 +0100

    Add PSRAM support for memory allocation
    - Introduced USE_PSRAM flag for conditional compilation
    - Updated memory allocation macros to use PSRAM functions
    - Retained original malloc/free behavior when PSRAM is not used

--- src/miniz.c
+++ src/miniz.c
@@ -14,10 +14,18 @@ typedef unsigned char mz_validate_uint64[sizeof(mz_uint64)==8 ? 1 : -1];
 
 #define MZ_ASSERT(x) assert(x)
 
+#define USE_PSRAM
+
+
 #ifdef MINIZ_NO_MALLOC
   #define MZ_MALLOC(x) NULL
   #define MZ_FREE(x) (void)x, ((void)0)
   #define MZ_REALLOC(p, x) NULL
+#elif USE_PSRAM
+  #include "esp_heap_caps.h"   
+  #define MZ_MALLOC(x) heap_caps_malloc(x, MALLOC_CAP_SPIRAM) 
+  #define MZ_FREE(x) free(x) 
+  #define MZ_REALLOC(p, x) heap_caps_realloc(p, x, MALLOC_CAP_SPIRAM) 
 #else
   #define MZ_MALLOC(x) malloc(x)
   #define MZ_FREE(x) free(x)
